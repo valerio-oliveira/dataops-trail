47441739a915c29e7cdcf8a0b0291a09ec1f7e29
valerio <valerio.net@gmail.com>
2021-11-22 22:16:33 -0300

    ansible provision postgres

--- aws/modules/databases/outputs.tf
+++ aws/modules/databases/outputs.tf
@@ -2,6 +2,10 @@ output "public_ip" {
   value = aws_instance.dbhost01.public_ip
 }
 
+output "private_ip" {
+  value = aws_instance.dbhost01.private_ip
+}
+
 output "public_dns" {
   value = aws_instance.dbhost01.public_dns
 }
