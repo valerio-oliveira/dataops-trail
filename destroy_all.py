abc046b08c71c9c4c9f2bb6efa7b8aaeb0f6b858
valerio <valerio.net@gmail.com>
2022-02-10 01:30:41 -0300

    Update readme.md

--- destroy_all.py
+++ destroy_all.py
@@ -1,5 +1,11 @@
 import os
 
+os.system('echo "--- Destroying" >> logs.txt')
+os.system('date >> logs.txt')
+
 os.chdir('terraform/aws/')
 os.system('terraform apply -auto-approve -destroy')
 os.chdir('../../')
+
+os.system('echo "--- End destroying" >> logs.txt')
+os.system('date >> logs.txt')
