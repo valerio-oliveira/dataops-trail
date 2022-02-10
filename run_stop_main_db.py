5bafa970bae56adabeffbcee63f101fd5d8e1501
valerio <valerio.net@gmail.com>
2022-02-09 21:31:29 -0300

    Fix broken directories references on tests

--- run_stop_main_db.py
+++ run_stop_main_db.py
@@ -1,6 +1,4 @@
 import os
-import time
-
 os.chdir('ansible/')
 os.system('echo "---------- STOP MAIN ----------" ; ansible-playbook -i inventories --forks 1 stop_main_db.yml; echo "-------------------------------"')
 os.chdir('../')
